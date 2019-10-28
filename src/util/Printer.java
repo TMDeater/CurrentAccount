c77c291a331698ee356be4593634b24d563dad7c
XDrzzz <14136263+TMDeater@users.noreply.github.com>
2019-10-28 10:36:30 +0800

    20191028 Update Printer.java

--- src/util/Printer.java
+++ src/util/Printer.java
@@ -14,7 +14,26 @@ public class Printer {
         Print("Account Balance: "+Double.toString(ac.getBalance()));
     }
 
-    public static void PrintPerformAction(){
-
+    public static void PrintOpenAccount(){
+        Print("1. Open Account");
+        Print("2. Exit");
     }
+
+    public static void PrintPerformAction(){
+        Print("1. Deposit");
+        Print("2. Withdraw");
+        Print("3. Close Account");
+    }
+
+    public static void PrintDepositWithdraw(int a, double amount, double amtAfterDeposit){
+        String action = new String();
+        if (a == 0){
+            action = "deposited";
+        } else if (a == 1){
+            action = "withdrawn";
+        }
+        Print("$"+amount + " had been "+action+" to your account,");
+        Print("Current balance: $"+ amtAfterDeposit);
+    }
+
 }
